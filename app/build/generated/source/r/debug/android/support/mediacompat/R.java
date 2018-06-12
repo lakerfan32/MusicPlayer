e23ab567a360ea30f7054138eb145fa1291e1452
RalphM <36464444+lakerfan32@users.noreply.github.com>
2018-06-12 00:47:52 -0400

    -Modified list_item file to include placeholder image in lists -Added different image files in drawable folders

--- app/build/generated/source/r/debug/android/support/mediacompat/R.java
+++ app/build/generated/source/r/debug/android/support/mediacompat/R.java
@@ -53,18 +53,18 @@ public final class R {
         public static final int notification_top_pad_large_text = 0x7f050069;
     }
     public static final class drawable {
-        public static final int notification_action_background = 0x7f060058;
-        public static final int notification_bg = 0x7f060059;
-        public static final int notification_bg_low = 0x7f06005a;
-        public static final int notification_bg_low_normal = 0x7f06005b;
-        public static final int notification_bg_low_pressed = 0x7f06005c;
-        public static final int notification_bg_normal = 0x7f06005d;
-        public static final int notification_bg_normal_pressed = 0x7f06005e;
-        public static final int notification_icon_background = 0x7f06005f;
-        public static final int notification_template_icon_bg = 0x7f060060;
-        public static final int notification_template_icon_low_bg = 0x7f060061;
-        public static final int notification_tile_bg = 0x7f060062;
-        public static final int notify_panel_notification_icon_bg = 0x7f060063;
+        public static final int notification_action_background = 0x7f06005a;
+        public static final int notification_bg = 0x7f06005b;
+        public static final int notification_bg_low = 0x7f06005c;
+        public static final int notification_bg_low_normal = 0x7f06005d;
+        public static final int notification_bg_low_pressed = 0x7f06005e;
+        public static final int notification_bg_normal = 0x7f06005f;
+        public static final int notification_bg_normal_pressed = 0x7f060060;
+        public static final int notification_icon_background = 0x7f060061;
+        public static final int notification_template_icon_bg = 0x7f060062;
+        public static final int notification_template_icon_low_bg = 0x7f060063;
+        public static final int notification_tile_bg = 0x7f060064;
+        public static final int notify_panel_notification_icon_bg = 0x7f060065;
     }
     public static final class id {
         public static final int action0 = 0x7f070006;
@@ -95,8 +95,8 @@ public final class R {
         public static final int status_bar_latest_event_content = 0x7f07007c;
         public static final int text = 0x7f070080;
         public static final int text2 = 0x7f070081;
-        public static final int time = 0x7f070084;
-        public static final int title = 0x7f070085;
+        public static final int time = 0x7f070085;
+        public static final int title = 0x7f070086;
     }
     public static final class integer {
         public static final int cancel_button_image_alpha = 0x7f080002;
